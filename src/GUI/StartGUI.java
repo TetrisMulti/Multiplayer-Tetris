08bfa5a08634a213e158c6652851a34fccab7e7d
Christoph <pomchb13@htl-kaindorf.ac.at>
2017-03-13 21:28:42 +0100

    Updatet Tetris GUI Added Closing Operation

--- src/GUI/StartGUI.java
+++ src/GUI/StartGUI.java
@@ -6,29 +6,45 @@ import java.awt.*;
 /**
  * Created by Christoph on 13.03.2017.
  */
-public class StartGUI extends JFrame{
+public class StartGUI extends JFrame {
+    protected static StartGUI gui=new StartGUI();
 
-
-    public StartGUI()
-    {
+    public StartGUI() {
         initComponents();
+
 
     }
 
     private void initComponents() {
         Dimension scrnSize = Toolkit.getDefaultToolkit().getScreenSize();
-        Rectangle winSize = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds();            // get Height of Taskbar
+        Rectangle winSize = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds();
         int taskBarHeight = scrnSize.height - winSize.height;
 
-        int width=(int)Toolkit.getDefaultToolkit().getScreenSize().getWidth()/3;
-        int height=((int)Toolkit.getDefaultToolkit().getScreenSize().getHeight()-taskBarHeight)/2;                     //get Height and Width of Screen
-        this.setSize(width,height);                                                                                //set Size of GUI
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);                                                              //
-        this.setLocationRelativeTo(null);                                                                          //set Location to the Center of the Screen
+        int width = (int) Toolkit.getDefaultToolkit().getScreenSize().getWidth() / 3;
+        int height = ((int) Toolkit.getDefaultToolkit().getScreenSize().getHeight() - taskBarHeight) / 2;
+        this.setSize(width, height);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+
+        JButton btStart = new JButton();
+        btStart.setText("Start");
+        Container cont = this.getContentPane();
+        cont.setLayout(new BorderLayout());
+        btStart.addActionListener((e) -> onStart());
+        cont.add(btStart, BorderLayout.CENTER);
+
     }
 
+    private void onStart() {
+        String nickname=JOptionPane.showInputDialog(this,"Bitte Nicknamen eingeben!");
+        TetrisGUI tGui = new TetrisGUI(nickname,gui);
+        tGui.setVisible(true);
+        this.setVisible(false);
+    }
+
+
     public static void main(String[] args) {
-        StartGUI gui = new StartGUI();
+
         gui.setVisible(true);
     }
 }
--- src/GUI/TetrisGUI.java
+++ src/GUI/TetrisGUI.java
@@ -2,6 +2,8 @@ package GUI;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 /**
  * Created by Chris on 13.03.2017.
@@ -11,24 +13,53 @@ import java.awt.*;
  */
 public class TetrisGUI extends JFrame{
 
-    private String nickName; 
-    public TetrisGUI(String nickName)
+    private String nickName;
+    private JFrame startGUI;
+    public TetrisGUI(String nickName,JFrame startGUI)
     {
+
         this.nickName=nickName;
+        this.startGUI=startGUI;
+        initialConfigs();
         initComponents();
     }
 
-    private void initComponents() {
-        Dimension scrnSize = Toolkit.getDefaultToolkit().getScreenSize();
-        Rectangle winSize = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds();            // get Height of Taskbar
-        int taskBarHeight = scrnSize.height - winSize.height;
+    private void initialConfigs() {
+        Dimension scrnSize = Toolkit.getDefaultToolkit().getScreenSize();                                        //get Screensize to set the application size dynamic
+        Rectangle winSize = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds();          // get Height of Taskbar
+        int taskBarHeight = scrnSize.height - winSize.height;                                                    // estiminate the height of the taskbar
+        this.setTitle(nickName+"'s Game");
+        int width=(int)Toolkit.getDefaultToolkit().getScreenSize().getWidth()/3;                                 //set the Width to a third of the screen size
+        int height=((int)Toolkit.getDefaultToolkit().getScreenSize().getHeight()-taskBarHeight);                 //set the Height of the frame without the taskbar
+        this.setSize(width,height);                                                                              //set Size of GUI
+        this.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);                                                      //disable the default close operation
+        this.setLocationRelativeTo(null);                                                                        //
+        this.addWindowListener(new WindowAdapter() {                                                             //Add a Actionlistener to rework the Close Operation
+            @Override
+            public void windowClosing(WindowEvent e) {
+                startGUI.setVisible(true);                                                                       //set the main gui visible again and close the current window
+                dispose();
 
-        int width=(int)Toolkit.getDefaultToolkit().getScreenSize().getWidth()/3;
-        int height=((int)Toolkit.getDefaultToolkit().getScreenSize().getHeight()-taskBarHeight)/2;                     //get Height and Width of Screen
-        this.setSize(width,height);                                                                                //set Size of GUI
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);                                                              //
-        this.setLocationRelativeTo(null);
+            }
+        });
     }
+
+    private void initComponents() {
+
+
+    }
+
+
+
+
+
+
+    /**private void onClose() {
+
+        startGUI.setTitle("normalerweise zu");
+        startGUI.setVisible(false);
+
+    }*/
 
 
 }
