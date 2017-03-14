4bd266a07bd9987a5c1d11eeee113b63b60ba831
ganleb13 <ganleb13@htl-kaindorf.ac.at>
2017-03-14 12:26:33 +0100

    Updated Starter GUI

---
+++ src/DB/PropertyLoader.java
@@ -0,0 +1,13 @@
+package DB;
+
+import java.io.File;
+
+/**
+ * Created by ganleb13 on 14.03.2017.
+ * In this class we load the Database-Properties from
+ */
+public class PropertyLoader {
+
+    public static final String PROPERTYFILE = System.getProperty("user.dir") + File.separator + "src" +
+            File.separator + "res" + File.separator;
+}
