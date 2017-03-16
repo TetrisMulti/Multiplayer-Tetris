f36cb7a4c1f5e6d5f2594d675ca3f0d0b5974fcf
ganleb13 <ganleb13@htl-kaindorf.ac.at>
2017-03-16 11:38:05 +0100

    GameOverGUI

---
+++ src/GUI/GameOverGUI.java
@@ -0,0 +1,18 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by ganleb13 on 16.03.2017.
+ * In this class we will define the GameOver-Screen
+ */
+public class GameOverGUI extends JFrame{
+
+    public void initComponents()
+    {
+        Container cont = this.getContentPane();
+    }
+}
+
+
