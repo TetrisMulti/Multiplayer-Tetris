9892827c18d290878fdb5bb244231b5243fd98cf
Hugo <hirhub13@htl-kaindorf.ac.at>
2017-03-20 14:45:19 +0100

    Updated the Design of the StarterGUI

--- src/GUI/StartGUI.java
+++ src/GUI/StartGUI.java
@@ -1,7 +1,11 @@
 package GUI;
 
+import res.Res;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
 
 /**
  * Created by Christoph on 13.03.2017.
@@ -11,7 +15,8 @@ public class StartGUI extends JFrame {
 
     public StartGUI() {
         initComponents();
-
+        this.setResizable(false);
+        this.setSize(500,500);
 
     }
 
@@ -27,8 +32,19 @@ public class StartGUI extends JFrame {
         this.setLocationRelativeTo(null);
 
         JPanel gesamtPanel = new JPanel();
-        gesamtPanel.setBackground(Color.red);
         JPanel panel1 = new JPanel();
+        JLabel ueberschrift = new JLabel();
+        gesamtPanel.setBackground(Color.lightGray);
+        JLabel gesamtLabel = new JLabel();
+        gesamtLabel.setSize(width, height);
+        gesamtLabel.setOpaque(true);
+        gesamtLabel.setBackground(Color.red);
+        try {
+            gesamtLabel.setIcon(new ImageIcon(ImageIO.read(Res.class.getResourceAsStream("tetris-movie.jpg"))));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        gesamtPanel.add(gesamtLabel);
 
 
         JButton btStart = new JButton();
@@ -43,26 +59,55 @@ public class StartGUI extends JFrame {
         JButton btHighScore = new JButton();
         btHighScore.setText("HighScore");
         panel1.add(btHighScore);
-
+        JButton btExit = new JButton();
+        btExit.setText("Beenden");
+        panel1.add(btExit);
 
 
         JButton btSettings = new JButton();
-        btSettings.setText("Settings");
-        panel1.add(btSettings);
+        try {
+            btSettings.setIcon(new ImageIcon(ImageIO.read(Res.class.getResourceAsStream("Settings.png"))));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        gesamtLabel.add(btSettings);
+        gesamtLabel.add(ueberschrift);
 
 
-        gesamtPanel.add(panel1);
+        gesamtLabel.add(panel1);
 
         Container cont = this.getContentPane();
         cont.setLayout(new BorderLayout());
         cont.add(gesamtPanel, BorderLayout.CENTER);
         gesamtPanel.setLayout(null);
         gesamtPanel.setSize(width, height);
-        panel1.setSize(gesamtPanel.getWidth()/5,gesamtPanel.getHeight()/3);
-        panel1.setLocation(gesamtPanel.getWidth()/2-gesamtPanel.getWidth()/8,gesamtPanel.getHeight()/2-gesamtPanel.getHeight()/3);
-        btStart.setSize(30,80);
+        panel1.setSize(gesamtPanel.getWidth()/2,gesamtPanel.getHeight()/2);
+        panel1.setLocation(gesamtPanel.getWidth()/3-125,gesamtPanel.getHeight()/2-200);
+        panel1.setLayout(null);
+        btStart.setSize(160,50);
+        btStart.setLocation(panel1.getWidth()/4, panel1.getHeight()/5-20);
+        btHighScore.setSize(160,50);
+        btHighScore.setLocation(panel1.getWidth()/4, panel1.getHeight()/2-25);
+        btSettings.setSize(35,35);
+        btSettings.setLocation(gesamtPanel.getWidth()/2-btSettings.getWidth(),gesamtPanel.getHeight()+btSettings.getHeight());
         btStart.addActionListener((e) -> onStart());
+        ueberschrift.setText("Tetris");
+        ueberschrift.setForeground(Color.yellow);
+        //ueberschrift.setOpaque(true);
+        ueberschrift.setSize(gesamtPanel.getWidth()+100,50);
+        //ueberschrift.setLocation(gesamtPanel.getWidth()+100, gesamtPanel.getHeight()/2-150);
+        ueberschrift.setFont(ueberschrift.getFont().deriveFont(40f));
+        btExit.setSize(160,50);
+        btExit.setLocation(panel1.getWidth()/4, panel1.getHeight()/2+50);
 
+
+        btExit.addActionListener(e -> {
+            try {
+                onExit();
+            } catch (Exception ex) {
+                System.out.println("Programm konnte nicht beendet werden");
+            }
+        });
 
         //cont.add(btStart);
         //cont.setLayout(null);
@@ -78,6 +123,10 @@ public class StartGUI extends JFrame {
         TetrisGUI tGui = new TetrisGUI(nickname,gui);
         tGui.setVisible(true);
         this.setVisible(false);
+    }
+
+    private void onExit() {
+        System.exit(0);
     }
 
 
---
+++ src/res/Res.java
@@ -0,0 +1,7 @@
+package res;
+
+/**
+ * Created by Hugo on 20.03.2017.
+ */
+public class Res {
+}
